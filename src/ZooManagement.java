b64690fc1a5025ff239bd048f56eed2cfd4b56f2
Yassine <benaichayassine50@gmail.com>
2024-10-06 17:10:23 +0100

    commit

---
+++ src/ZooManagement.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ZooManagement {
+    int nbrCages = 20;
+    String zooName = "myZoo";
+
+    public static void main(String[] args) {
+        //Instruction 1
+        ZooManagement z = new ZooManagement();
+        System.out.println(z.zooName + " comporte " + z.nbrCages);
+
+
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir le nombre de cages souhaité:");
+        int nbrCagesSaisie = sc.nextInt();
+        ZooManagement z1 = new ZooManagement();
+        z1.nbrCages = nbrCagesSaisie;
+
+        z1.zooName = "MyZoo1";
+        System.out.println(z1.zooName + " comporte " + z1.nbrCages);
+    }
+}
